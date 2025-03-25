4df6b3b7bcdab0d98e2c2e7c128075a69d2f5359
Aba <abarajithan07@gmail.com>
2025-03-24 20:08:12 -0700

    New set of experiments

--- weights_on_chip/run_microbenchmarking.sh
+++ weights_on_chip/run_microbenchmarking.sh
@@ -2,21 +2,21 @@
 set -e
 
 # Define arrays with given values
-M_vals=(4 8 8 8 8 32 32 32 32 8 16 32 64 128)
-K_vals=(8 16 32 64 128 16 32 64 128 8 16 32 64 128)
-N_vals=(4 8 16 16 32 8 16 16 32 8 16 32 64 128)
-m_vals=(4 4 4 4 4 4 4 4 4 2 2 2 2 2)
-k_vals=(8 8 8 8 8 8 8 8 8 8 8 8 8 8)
-n_vals=(4 4 4 4 4 4 4 4 4 8 8 8 8 8)
+# M_vals=(4 8 8 8 8 32 32 32 32 8 16 32 64 128)
+K_vals=(16 32 64 16 32 256 16 64 16 64 128 512 128)
+N_vals=(8 16 16 64 32 16 256 64 512 128 64 16 128)
+# m_vals=(4 4 4 4 4 4 4 4 4 2 2 2 2 2)
+# k_vals=(8 8 8 8 8 8 8 8 8 8 8 8 8 8)
+# n_vals=(4 4 4 4 4 4 4 4 4 8 8 8 8 8)
 
 # Iterate through the arrays and call make
-for i in "${!M_vals[@]}"; do
-    M=${M_vals[$i]}
+for i in "${!K_vals[@]}"; do
+    M=2
     K=${K_vals[$i]}
     N=${N_vals[$i]}
-    m=${m_vals[$i]}
-    k=${k_vals[$i]}
-    n=${n_vals[$i]}
+    m=2
+    k=8
+    n=8
 
     echo "Running make with: i=$i M=$M K=$K N=$N m=$m k=$k n=$n"
 
