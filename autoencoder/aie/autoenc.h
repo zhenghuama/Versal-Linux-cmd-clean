5ee4e6c2546f82d788e4d025e1a837a391cc5726
Aba <abarajithan07@gmail.com>
2025-03-24 18:43:38 -0700

    Add 8 layer autoencoder

--- autoencoder/aie/autoenc.h
+++ autoencoder/aie/autoenc.h
@@ -1,8 +1,19 @@
 
 #ifndef AUTOENC_H
 #define AUTOENC_H
-	
-	void f0(input_window_int8 * __restrict matA, output_window_int8 * __restrict matC);
-	void f1(input_window_int8 * __restrict matA, output_window_int8 * __restrict matC);
+
+#define DENSE_FN(IDX) void f##IDX(input_window_int8 * __restrict matA, output_window_int8 * __restrict matC);
+
+DENSE_FN(0)
+DENSE_FN(1)
+DENSE_FN(2)
+DENSE_FN(3)
+DENSE_FN(4)
+DENSE_FN(5)
+DENSE_FN(6)
+DENSE_FN(7)
+DENSE_FN(8)
+
+#undef DENSE_FN
 
 #endif
--- autoencoder/aie/graph.cpp
+++ autoencoder/aie/graph.cpp
@@ -3,7 +3,7 @@
 #include "autoenc.h"
 #include <vector>
 
-#define N_LAYERS 2
+#define N_LAYERS 9
 
 using namespace adf;
 
@@ -23,11 +23,25 @@ public:
 	  // kernels creation
 		layers[0] = kernel::create(f0);
 		layers[1] = kernel::create(f1);
+		layers[2] = kernel::create(f2);
+		layers[3] = kernel::create(f3);
+		layers[4] = kernel::create(f4);
+		layers[5] = kernel::create(f5);
+		layers[6] = kernel::create(f6);
+		layers[7] = kernel::create(f7);
+		layers[8] = kernel::create(f8);
 
 	  // Single kernel connections
 	  connect< window<2*128*1> >  (A        .out[0], layers[0].in[0]);
 	  connect< window<2*128*1> >  (layers[0].out[0], layers[1].in[0]);
-	  connect< window<2*128*1> >  (layers[1].out[0], C        .in[0]);
+	  connect< window<2*128*1> >  (layers[1].out[0], layers[2].in[0]);
+	  connect< window<2*128*1> >  (layers[2].out[0], layers[3].in[0]);
+	  connect< window<2*  8*1> >  (layers[3].out[0], layers[4].in[0]);
+	  connect< window<2*  8*1> >  (layers[4].out[0], layers[5].in[0]);
+	  connect< window<2*128*1> >  (layers[5].out[0], layers[6].in[0]);
+	  connect< window<2*128*1> >  (layers[6].out[0], layers[7].in[0]);
+	  connect< window<2*128*1> >  (layers[7].out[0], layers[8].in[0]);
+	  connect< window<2*128*1> >  (layers[8].out[0], C        .in[0]);
 
 	  // direct the source file of kernels
 		for (int i=0; i<N_LAYERS; i++) {
